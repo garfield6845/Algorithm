4f8adaaa5f9913a78b437b233ddccfa3ab47c479
조윤지 <garfield6845@gmail.com>
2024-03-25 09:58:15 +0000

    3-25

--- baekjoon/1008/1008.c
+++ baekjoon/1008/1008.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNINGS
+
 int main(){
     int a,b;
-    scanf("%d %d",&a,&b);
-    printf("%d",a-b);
-    return 0;
+    scanf("%d %d");
+    printf("%f",a/b);
 }
