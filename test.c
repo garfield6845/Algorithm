2db88a742c8551e65e70cbb939af61297bdd587a
조윤지 <garfield6845@gmail.com>
2024-04-04 20:21:29 +0900

    4-4

--- test.c
+++ test.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-int main(void)
-{
-    int num=1,sum=0;
-    
+void sum(int *a,int *b,int *c){
+    *c=*a+*b;
+}
+
+int main(){
+    int a=1,b=3,c=0;
+    int *ptrA=&a;
+    int *ptrB=&b;
+    int *ptrC=&c;
+    sum(ptrA,ptrB,ptrC);
+    printf("%d\n",c);
 }
 
 
-//저장하고 gcc (파일 이름) 하고 ./a.out 하기 
+//저장하고 gcc (파일 이름) 하고 ./a.out 하기  
