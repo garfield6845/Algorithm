1d0ec8d3125b1ef55477379b31552269143f2fca
조윤지 <garfield6845@gmail.com>
2024-04-04 20:42:08 +0900

    4-4

--- test.c
+++ test.c
@@ -4,8 +4,14 @@
 
 int main(){
     char arr[10];
+    char arr2[10]="123456789";
     scanf("%s",arr);
+    printf("%lld\n",arr);
+    printf("%lld\n",arr2);
     printf("%s\n",arr);
+    printf("%s\n",arr2);
+    for(int i=0;i<10;i++)
+        printf("%c",arr[i]);
 }
 
 
