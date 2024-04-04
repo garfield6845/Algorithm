881c91c5f598799e5fb9bed2863e473f4158ae06
조윤지 <garfield6845@gmail.com>
2024-04-04 20:29:34 +0900

    4-4

--- test.c
+++ test.c
@@ -2,17 +2,10 @@
 #include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-void sum(int *a,int *b,int *c){
-    *c=*a+*b;
-}
-
 int main(){
-    int a=1,b=3,c=0;
-    int *ptrA=&a;
-    int *ptrB=&b;
-    int *ptrC=&c;
-    sum(ptrA,ptrB,ptrC);
-    printf("%d\n",c);
+    char arr[10];
+    scanf("%s",arr);
+    printf("%s\n",arr);
 }
 
 
